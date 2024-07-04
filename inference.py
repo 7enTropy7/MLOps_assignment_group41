31d508a511a310c8fc20e8263a541a5c73419d77
Unnikrishnan Menon <menon.uk1998@gmail.com>
2024-07-05 02:23:28 +0530

    inference script improved

--- inference.py
+++ inference.py
@@ -1,21 +1,28 @@
+"""
+Perform Inference on the Trained Model.
+"""
 import joblib
 import numpy as np
 
-model_filename = 'mlp_regressor_model.pkl'
-scaler_filename = 'scaler.pkl'
-loaded_mlp = joblib.load(model_filename)
-loaded_scaler = joblib.load(scaler_filename)
+MODEL_FILENAME = 'mlp_regressor_model.pkl'
+SCALER_FILENAME = 'scaler.pkl'
+
+loaded_mlp = joblib.load(MODEL_FILENAME)
+loaded_scaler = joblib.load(SCALER_FILENAME)
 
 def run_inference(new_data):
-    if len(new_data.shape) == 1:
+    """
+    Runs inference using the loaded MLP model on new data.
+    """
+    if new_data.ndim == 1:
         new_data = new_data.reshape(1, -1)
     new_data_scaled = loaded_scaler.transform(new_data)
-    predictions = loaded_mlp.predict(new_data_scaled)    
+    predictions = loaded_mlp.predict(new_data_scaled)
     return predictions
 
 if __name__ == "__main__":
-    example_data = np.array([0.00632, 18.0, 2.31, 0.0, 0.538, 6.575, 65.2, 4.0900, 1.0, 296.0, 15.3, 396.90, 4.98])
-    
+    example_data = np.array([0.00632, 18.0, 2.31, 0.0, 0.538, 6.575,
+                             65.2, 4.0900, 1.0, 296.0, 15.3, 396.90, 4.98])
+
     prediction = run_inference(example_data)
-    print('Prediction for example data: {}'.format(prediction))
-    
+    print(f'Prediction for example data: {prediction}')
