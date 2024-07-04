4687090ff3a05ba06b6aae252580a9ac99afde6d
Unnikrishnan Menon <menon.uk1998@gmail.com>
2024-07-05 02:17:50 +0530

    improvements to client code

--- client.py
+++ client.py
@@ -1,7 +1,18 @@
+"""
+Script to send a POST request with JSON data 
+to a local server for prediction.
+"""
 import requests
 
-data = {'features': [0.00632, 18.0, 2.31, 0.0, 0.538, 6.575, 65.2, 4.0900, 1.0, 296.0, 15.3, 396.90, 4.98]}
+data = {
+    'features': [
+        0.00632, 18.0, 2.31, 0.0,
+        0.538, 6.575, 65.2, 4.0900,
+        1.0, 296.0, 15.3, 396.90, 4.98
+    ]
+}
 
-response = requests.post('http://127.0.0.1:5000/predict', json=data)
+URL = 'http://127.0.0.1:5000/predict'
+response = requests.post(URL, json=data, timeout=1.5)
 
 print(response.json())
