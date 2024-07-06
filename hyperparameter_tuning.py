0e62cb264711e404199d616262d20c98c14b1818
Unnikrishnan Menon <menon.uk1998@gmail.com>
2024-07-06 20:20:25 +0530

    saves best model based on optuna results

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -7,6 +7,7 @@ import warnings
 import optuna
 from optuna_dashboard import run_server
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
@@ -23,10 +24,17 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_valid = scaler.transform(X_valid)
 
+SCALER_FILENAME = 'scaler.pkl'
+joblib.dump(scaler, SCALER_FILENAME)
+
+BEST_MODEL = None
+GLOBAL_MODEL = None
+
 def objective(trial):
     """
     Objective function for Optuna to perform hyperparameter-tuning.
     """
+    global GLOBAL_MODEL
     params = {
         'learning_rate_init': trial.suggest_float('learning_rate_init ', 0.0001, 0.1, step=0.005),
         'first_layer_neurons': trial.suggest_int('first_layer_neurons', 10, 100, step=10),
@@ -44,10 +52,23 @@ def objective(trial):
 
     model.fit(X_train, y_train)
 
+    GLOBAL_MODEL = model
+
     return mean_squared_error(y_valid, model.predict(X_valid), squared=False)
+
+def callback(study, trial):
+    """
+    Callback function for updating best model
+    """
+    global BEST_MODEL
+    if study.best_trial == trial:
+        BEST_MODEL = GLOBAL_MODEL
 
 storage = optuna.storages.InMemoryStorage()
 study = optuna.create_study(storage=storage, direction='minimize')
-study.optimize(objective, n_trials=3)
+study.optimize(objective, n_trials=3, callbacks=[callback])
+
+MODEL_FILENAME = 'best_model.pkl'
+joblib.dump(BEST_MODEL, MODEL_FILENAME)
 
 run_server(storage)
